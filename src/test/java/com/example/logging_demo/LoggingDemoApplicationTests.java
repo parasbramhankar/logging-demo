b81fa6fc58b38368804ea185b2af3bf5fd1cba14
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-10 11:08:14 +0530

    commit

---
+++ src/test/java/com/example/logging_demo/LoggingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logging_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
